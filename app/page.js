21168a243212efc3f71fc5c7129b5f6dcbcbd397
DMitch94 <dlmitche@syr.edu>
2024-03-26 17:22:50 -0400

    Add count state to Homepage component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 import Button from "../components/Button";
-
 const Homepage = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Weather app</h1>
+      <h2>Count: {count}</h2>
       <Button label="Download" />
       <Button label="Register now" />
       <Button label="Learn more" />
