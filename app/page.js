0ed1cdb604a8b9a532bf8bb78e641809dcb54bd3
DMitch94 <dlmitche@syr.edu>
2024-03-28 18:33:07 -0400

    Add color picker functionality to ColorPicker component

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import ColorPicker from "../components/ColorPicker";
 
 const Homepage = () => {
   const [count, setCount] = useState(0);
-  const [isVisible, setIsVisible] = useState();
+  const [isVisible, setIsVisible] = useState(False);
 
   return (
     <div>
