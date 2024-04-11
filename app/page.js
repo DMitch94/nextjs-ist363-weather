41b5a8bbb058e5f9a78b56e69fdfed2623ef74db
DMitch94 <dlmitche@syr.edu>
2024-04-11 16:57:22 -0400

    environment weather addin

--- app/page.js
+++ app/page.js
@@ -8,7 +8,12 @@ import ButtonDemo from "../components/ButtonDemo";
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "../components/PeoplePicker";
 
-import { getGeoLocation, getPeople, getWeatherData } from "../lib/api";
+import {
+  getGeoLocation,
+  getPeople,
+  getWeatherData,
+  getWeatherDataByLatLon,
+} from "../lib/api";
 
 const Homepage = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -21,42 +26,47 @@ const Homepage = () => {
 
   useEffect(() => {
     getGeoLocation()
-    .then((position)) => {
-      setLocation(position);
-    }.catch();
+      .then((position) => {
+        console.log(position);
+        setLocation(position);
+      })
+      .catch((error) => {
+        setErrorMsg(error);
+      });
   }, []);
 
-useEffect(() => {
-  // filter out the days of the week
-  const tempWeek =[];
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getWeatherDataByLatLon(location);
+      setWeatherData(response);
+    };
+    location ? fetchData() : null;
+  }, [location]);
 
-  weatherData && 
-    weatherData.list.filter((block) => {
-      const date = new Date(block.dt * 1000);
-      const options = { weekday: "short" };
-      const day = date.toLocaleDateString("en-US", options);
-      //console.log(day);
-      if (!tempWeek.includes(day)) {
-        tempWeek.push(day);
-      }
-  });
+  useEffect(() => {
+    // filter out the days of the week
+    const tempWeek = [];
 
-  setdaysOFWeek(tempWeek);
-  // then set state with the days of the week
-}, [weatherData]);
+    weatherData &&
+      weatherData.list.filter((block) => {
+        const date = new Date(block.dt * 1000);
+        const options = { weekday: "short" };
+        const day = date.toLocaleDateString("en-US", options);
+        //console.log(day);
+        if (!tempWeek.includes(day)) {
+          tempWeek.push(day);
+        }
+      });
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await getWeatherData();
-  //     setWeatherData(response);
-  //   };
-  //   fetchData();
-  // }, []);
+    setdaysOFWeek(tempWeek);
+    // then set state with the days of the week
+  }, [weatherData]);
 
   //console.log(peopleArr);
   return (
     <div>
       <h1>Weather app</h1>
+      {errorMsg && <div>{errorMsg}</div>}
       {weatherData && (
         <div>
           <h2>{weatherData.city.name}</h2>
@@ -73,25 +83,27 @@ useEffect(() => {
       {/*<PeoplePicker people={peopleArr} />
       <ButtonDemo />
   <ColorPicker />*/}
-    {daysOfWeek&& (<section>
-      <ul>
-      {daysOfWeek?.map((day, index) => {
-        return <li key={index}>{day}</li>
-      })}
-      </ul>
-      <div>{weatherData.list
-      .filter((block) => {
-        const date = new Date(block.dt * 1000);
-        const options = { weekday: "short" };
-        const day = date.toLocaleDateString("en-US", options);
-        return day === daysOfWeek[activeDayIndex];
-      })
-      .map((block, index) => {
-        return <p key={index}>{block.main.temp}</p>;
-      })}
-      </div>
-      </section>
-    )}
+      {daysOfWeek && (
+        <section>
+          <ul>
+            {daysOfWeek?.map((day, index) => {
+              return <li key={index}>{day}</li>;
+            })}
+          </ul>
+          <div>
+            {weatherData.list
+              .filter((block) => {
+                const date = new Date(block.dt * 1000);
+                const options = { weekday: "short" };
+                const day = date.toLocaleDateString("en-US", options);
+                return day === daysOfWeek[activeDayIndex];
+              })
+              .map((block, index) => {
+                return <p key={index}>{block.main.temp}</p>;
+              })}
+          </div>
+        </section>
+      )}
     </div>
   );
 };
