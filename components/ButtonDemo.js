df5972ccb60a3d1b9a39c6cd0bec31367aa902d9
DMitch94 <dlmitche@syr.edu>
2024-04-17 15:10:38 -0400

    fixed button imports

--- components/ButtonDemo.js
+++ components/ButtonDemo.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import Button from "../components/Button";
+import Button from "./Button";
 
 const ButtonDemo = () => {
   const [count, setCount] = useState(0);
